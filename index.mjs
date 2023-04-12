004efb33045f6c7ed425ba4e8be4995289c75d83
Vikas Pathak <123vikas.vp79@gmail.com>
2023-04-13 01:13:31 +0530

    change env variable again

--- index.mjs
+++ index.mjs
@@ -4,11 +4,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const hostname = process.env.HOSTNAME||"localhost";
-const port = process.env.PORT||8080; 
+// const hostname = process.env.HOSTNAME||"localhost";
+// const port = process.env.PORT||8080; 
 // console.log(hostname);
 // console.log(port);
  
-app.listen(port,hostname, () => {
-  console.log(`Server running at http://${hostname}/:${port}/`);
-});   
+app.listen(8080, () => console.log('Server running...... at 8080'));
